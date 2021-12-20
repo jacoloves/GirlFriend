02a2180c461c3a4b749cc50f97695bd397896e7b
jacoloves <5511068t@gmail.com>
2021-12-21 07:29:19 +0900

    first commit

---
+++ GirlFriend/Cchan.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GirlFriend
+{
+    internal class Cchan
+    {
+        private string _name;
+        private Cdictionary _dictionary;
+        private CchanEmotion _emotion;
+        private RandomResponder _res_random;
+        private RepeatResponder _res_repeat;
+        private PatternResponder _res_pattern;
+        private Responder _responder;
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public CchanEmotion Emotion
+        {
+            get => _emotion;
+        }
+
+        public Cchan(string name)
+        {
+            _name = name;
+            _dictionary = new Cdictionary();
+            _emotion = new CchanEmotion(_dictionary);
+            _res_repeat = new RepeatResponder("Repeat", _dictionary);
+            _res_random = new RandomResponder("Random", _dictionary);
+            _res_pattern = new PatternResponder("Pattern", _dictionary);
+            _responder = new Responder("Responder", _dictionary);
+        }
+
+        public string Dialogue(string input)
+        {
+            _emotion.Update(input);
+            // {形態素、名詞情報}の配列を格納したリストを取得
+            List<string[]> parts = Analyzer.Analyze(input);
+            Random rnd = new();
+            int num = rnd.Next(0, 10);
+            
+            if (Regex.IsMatch(input, "メリークリスマス[!|！]*"))
+                _responder = _res_pattern;
+            else if (num < 6)
+                _responder = _res_pattern;
+            else if(num < 9)
+                _responder = _res_random;
+            else
+                _responder = _res_repeat;
+
+            string resp = _responder.Response(
+                input,
+                _emotion.Mood);
+
+            _dictionary.Study(input, parts);
+
+            return resp;
+        }
+
+        public string GetName()
+        {
+            return _responder.Name;
+        }
+
+        public void Save()
+        {
+            _dictionary.Save();
+        }
+    }
+}
---
+++ GirlFriend/Cdictionary.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GirlFriend
+{
+    internal class Cdictionary
+    {
+        private List<string> _randomList = new();
+
+        private List<ParseItem> _patternList = new();
+
+        public List<string> Random
+        { get => _randomList; }
+
+        public List<ParseItem> Pattern
+        { get => _patternList; }
+
+        public Cdictionary()
+        {
+            MakeRandomList();
+            MakePatternList();
+        }
+
+        private void MakeRandomList()
+        { 
+            string[] r_lines = File.ReadAllLines(
+                @"dics\random.txt",
+                System.Text.Encoding.UTF8
+                );
+
+            foreach (string line in r_lines)
+            {
+                string str = line.Replace("\n", "");
+                if (line != "")
+                {
+                    _randomList.Add(str);
+                }
+            } 
+        }
+
+        private void MakePatternList()
+        { 
+            string[] p_lines = File.ReadAllLines(
+                @"dics\pattern.txt",
+                System.Text.Encoding.UTF8
+                );
+
+            List<string> new_lines = new();
+
+            foreach (string line in p_lines)
+            {
+                string str = line.Replace("\n", "");
+                if (line != "")
+                {
+                    new_lines.Add(str);
+                }
+            }
+
+            foreach (string line in new_lines)
+            {
+                // add code
+                // line replace \\t -> \t
+                string rep_line = line.Replace("\\t", "\t");
+                string[] carveLine = rep_line.Split(new char[] { '\t' });
+                _patternList.Add(
+                    new ParseItem(
+                        carveLine[0],
+                        carveLine[1])
+                    );
+            }
+        }
+
+        public void Study(string input, List<string[]> parts)
+        {
+            string userInput = input.Replace("\n", "");
+            StudyRandom(userInput);
+            StudyPattern(userInput, parts);
+        }
+
+        // ランダム辞書に追加する
+        public void StudyRandom(string userInput)
+        {
+            if (_randomList.Contains(userInput) == false)
+            {
+                _randomList.Add(userInput);
+            }
+        }
+
+        // パターン辞書に追加する
+        public void StudyPattern(string userInput, List<string[]> parts)
+        {
+            foreach (string[] morpheme in parts)
+            {
+                if (Analyzer.KeyWordCheck(morpheme[1]).Success)
+                {
+                    ParseItem? depend = null;
+
+                    foreach (ParseItem item in _patternList)
+                    {
+                        if (!string.IsNullOrEmpty(item.Match(userInput)))
+                        {
+                            depend = item;
+                            break;
+                        }
+                    }
+
+                    if (depend != null)
+                    {
+                        depend.AddPhrase(userInput);
+                    }
+                    else
+                    {
+                        _patternList.Add(new ParseItem(
+                            morpheme[0],
+                            userInput));
+                    }
+                }
+            }
+        }
+
+        public void Save()
+        {
+            File.WriteAllLines(
+                @"dics\random.txt",
+                _randomList,
+                System.Text.Encoding.UTF8
+                );
+
+            List<string> patternLine = new();
+
+            foreach (ParseItem item in _patternList)
+            {
+                patternLine.Add(item.MakeLine());
+            }
+
+            File.WriteAllLines(
+                @"dics\test_pattern.txt",
+                patternLine,
+                System.Text.Encoding.UTF8
+                );
+        }
+    }
+}

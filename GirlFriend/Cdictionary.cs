ac00f7b92136dce43eb1ad3a5bff7065c17c3073
jacoloves <5511068t@gmail.com>
2021-12-23 07:47:18 +0900

    template study1

--- GirlFriend/Cdictionary.cs
+++ GirlFriend/Cdictionary.cs
@@ -13,16 +13,22 @@ namespace GirlFriend
 
         private List<ParseItem> _patternList = new();
 
+        Dictionary<string, List<string>> _templateDictionary = new();
+
         public List<string> Random
         { get => _randomList; }
 
         public List<ParseItem> Pattern
         { get => _patternList; }
 
+        public Dictionary<string, List<string>> Template
+        { get => _templateDictionary; }
+
         public Cdictionary()
         {
             MakeRandomList();
             MakePatternList();
+            MakeTemplateDictionary();
         }
 
         private void MakeRandomList()
@@ -74,11 +80,48 @@ namespace GirlFriend
             }
         }
 
+        // テンプレート辞書を作成する。
+        private void MakeTemplateDictionary()
+        {
+            string[] t_lines = File.ReadAllLines(
+                @"dics\template.txt",
+                System.Text.Encoding.UTF8
+                );
+
+            List<string> new_lines = new();
+
+            foreach (string line in t_lines)
+            {
+                string str = line.Replace("\n", "");
+
+                if (line != "")
+                {
+                    new_lines.Add(str);
+                }
+            }
+
+            foreach (string line in new_lines)
+            {
+                // add code
+                // line replace \\t -> \t
+                string rep_line = line.Replace("\\t", "\t");
+                string[] carveLine = rep_line.Split(new Char[] { '\t' });
+
+                if (!_templateDictionary.ContainsKey(carveLine[0]))
+                {
+                    _templateDictionary.Add(carveLine[0], new List<string> { });
+                }
+
+                _templateDictionary[carveLine[0]].Add(carveLine[1]);
+            }
+        }
+
         public void Study(string input, List<string[]> parts)
         {
             string userInput = input.Replace("\n", "");
             StudyRandom(userInput);
             StudyPattern(userInput, parts);
+            StudyTemplate(parts);
         }
 
         // ランダム辞書に追加する
@@ -122,6 +165,39 @@ namespace GirlFriend
             }
         }
 
+        // ユーザーの発言をテンプレート学習して、テンプレート辞書に追加する。
+        public void StudyTemplate(List<string[]> parts)
+        {
+            string tempStr = "";
+            int count = 0;
+
+            foreach (string[] morpheme in parts)
+            {
+                if (Analyzer.KeyWordCheck(morpheme[1]).Success)
+                {
+                    morpheme[0] = "%noun%";
+                    count++;
+                }
+
+                tempStr += morpheme[0];
+            }
+
+            if (count > 0)
+            {
+                string num = Convert.ToString(count);
+
+                if (!_templateDictionary.ContainsKey(num))
+                {
+                    _templateDictionary.Add(num, new List<string>());
+                }
+
+                if (!_templateDictionary[num].Contains(tempStr))
+                { 
+                    _templateDictionary[num].Add(tempStr);
+                }
+            }
+        }
+
         public void Save()
         {
             File.WriteAllLines(
@@ -142,6 +218,24 @@ namespace GirlFriend
                 patternLine,
                 System.Text.Encoding.UTF8
                 );
+
+            List<string> templateLine = new();
+
+            foreach (var dic in _templateDictionary)
+            {
+                foreach (var temp in dic.Value)
+                {
+                    templateLine.Add(dic.Key + "\\t" + temp);
+                }
+
+                templateLine.Sort();
+
+                File.WriteAllLines(
+                    @"dics\template.txt",
+                    templateLine,
+                    System.Text.Encoding.UTF8
+                    );
+            }
         }
     }
 }
---
+++ GirlFriend/TemplateResponder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace GirlFriend
+{
+    internal class TemplateResponder : Responder
+    {
+        public TemplateResponder(string name, Cdictionary dic) : base(name, dic)
+        { 
+        }
+
+        public override string Response(string input, int mood, List<string[]> parts)
+        {
+            List<string> keywords = new();
+
+            foreach (string[] morpheme in parts)
+            {
+                if (Analyzer.KeyWordCheck(morpheme[1]).Success)
+                { 
+                    keywords.Add(morpheme[0]);
+                }
+            }
+
+            Random rdm = new(Environment.TickCount);
+
+            int count = keywords.Count;
+
+            if ((count > 0) && (Cdictionary.Template.ContainsKey(Convert.ToString(count))))
+            { 
+                var templates = Cdictionary.Template[Convert.ToString(count)];
+                string temp = templates[rdm.Next(0, templates.Count)];
+
+                Regex re = new("%noun%");
+
+                foreach (string word in keywords)
+                {
+                    temp = re.Replace(temp, word, 1);
+                }
+
+                return temp;
+            }
+
+            return Cdictionary.Random[rdm.Next(0, Cdictionary.Random.Count)];
+        }
+    }
+}
